b50d8b94c301af636fa8e4b1ee73a6e2656108c7
Marcin Medryk <medryk.kontakt@gmail.com>
2020-11-13 01:12:13 +0100

    nwm co robie

--- src/Domino.js
+++ src/Domino.js
@@ -3,27 +3,50 @@ import "./Domino.css";
 
 function Domino(props) {
   let [color, setColor] = React.useState("red");
-  let [isStarted, setIsStarted] = React.useState(false);
+  // let [isStarted, setIsStarted] = React.useState(false);
+  let [newDominoActive, setNewDominoActive] = React.useState([]);
+  let [newNumber, setNewNumber] = React.useState(0);
 
   let onClick = () => {
-    if (isStarted === true) {
-      return;
+    // if (isStarted === true) {
+    //   return;
+    // }
+    // setIsStarted(true);
+    if (newDominoActive[0] === "red") {
+      setTimeout(function () {
+        setNewDominoActive(newDominoActive.map((x) => "blue"));
+      }, 2000);
+    } else {
+      setNewDominoActive(newDominoActive.map((x) => "red"));
     }
-    setIsStarted(true);
-
+    console.log(newDominoActive);
     setTimeout(function () {
       setColor((oldColor) => (oldColor === "blue" ? "red" : "blue"));
-    }, 1500);
+    }, 1);
+  };
+  let onButtonClick = () => {
+    setNewDominoActive([...newDominoActive, "red"]);
+    setNewNumber(newNumber + 1);
   };
 
-  React.useEffect(() => {
-    let timeoutID = setTimeout(function () {
-      setColor((oldColor) => (oldColor === "blue" ? "red" : "blue"));
-    }, 1000);
-    return () => clearTimeout(timeoutID);
-  }, [color]);
-
-  return <div onClick={onClick} className={"domino " + color}></div>;
+  return (
+    <div>
+      <div onClick={onClick} className={"domino " + color}></div>
+      <button onClick={onButtonClick} className="button">
+        +
+      </button>
+      {newDominoActive.map((x) => (
+        <div className={"domino " + x} key={Math.random(0, 1)} number={x} />
+      ))}
+    </div>
+  );
 }
 
 export default Domino;
+
+// React.useEffect(() => {
+//   let timeoutID = setTimeout(function () {
+//     setColor((oldColor) => (oldColor === "blue" ? "red" : "blue"));
+//   }, 1000);
+//   return () => clearTimeout(timeoutID);
+// }, [color]);
