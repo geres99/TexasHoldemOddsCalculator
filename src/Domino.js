bfdb271bfcc3946ab1a5cb6f0ffefab05cce1bf7
Marcin Medryk <medryk.kontakt@gmail.com>
2020-11-14 13:08:01 +0100

    przegranko

--- src/Domino.js
+++ src/Domino.js
@@ -2,27 +2,50 @@ import React from "react";
 import "./Domino.css";
 
 function Domino(props) {
-  let [color, setColor] = React.useState("red");
-  // let [isStarted, setIsStarted] = React.useState(false);
   let [newDominoActive, setNewDominoActive] = React.useState([]);
   let [newNumber, setNewNumber] = React.useState(0);
+  let [isTimerActive, setIsTimerActive] = React.useState(false);
+  React.useEffect(() => {
+    let timeOutFunction = () => {
+      if (!isTimerActive) {
+        return;
+      }
+      let isChanged = false;
+      let CopyOfDomino = newDominoActive.slice();
+      for (let i = 0; i < CopyOfDomino.length; i++) {
+        if (newDominoActive[i] === "blue") {
+          if (newDominoActive[i + 1] === "red") {
+            CopyOfDomino[i + 1] = "blue";
+            isChanged = true;
+          }
+        }
+      }
+      if (!isChanged) {
+        setIsTimerActive(false);
+      } else {
+        setTimeout(timeOutFunction, 500);
+      }
+      // setNewDominoActive(CopyOfDomino);
+    };
 
-  let onClick = () => {
-    // if (isStarted === true) {
-    //   return;
-    // }
-    // setIsStarted(true);
-    if (newDominoActive[0] === "red") {
-      setTimeout(function () {
-        setNewDominoActive(newDominoActive.map((x) => "blue"));
-      }, 2000);
-    } else {
-      setNewDominoActive(newDominoActive.map((x) => "red"));
+    setTimeout(timeOutFunction, 500);
+  }, [isTimerActive]);
+
+  let onClick = (e) => {
+    setIsTimerActive(true);
+    let uniqueNumber = e.target.attributes.number.nodeValue;
+    let CopyOfDomino = newDominoActive.slice();
+
+    for (let i = 0; i < CopyOfDomino.length; i++) {
+      if (uniqueNumber === i.toString()) {
+        if (CopyOfDomino[i] === "red") {
+          CopyOfDomino[i] = "blue";
+        } else {
+          CopyOfDomino[i] = "red";
+        }
+      }
     }
-    console.log(newDominoActive);
-    setTimeout(function () {
-      setColor((oldColor) => (oldColor === "blue" ? "red" : "blue"));
-    }, 1);
+    setNewDominoActive(CopyOfDomino);
   };
   let onButtonClick = () => {
     setNewDominoActive([...newDominoActive, "red"]);
@@ -31,12 +54,17 @@ function Domino(props) {
 
   return (
     <div>
-      <div onClick={onClick} className={"domino " + color}></div>
+      <div className={"domino red"}></div>
       <button onClick={onButtonClick} className="button">
         +
       </button>
-      {newDominoActive.map((x) => (
-        <div className={"domino " + x} key={Math.random(0, 1)} number={x} />
+      {newDominoActive.map((x, y) => (
+        <div
+          onClick={onClick}
+          className={"domino " + x}
+          number={y}
+          key={Math.random(0, 1)}
+        />
       ))}
     </div>
   );
