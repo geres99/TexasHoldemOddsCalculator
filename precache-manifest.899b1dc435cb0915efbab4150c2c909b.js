81add6a60774ba63926525877b2264dc0a3c9d22
Marcin Medryk <medryk.kontakt@gmail.com>
2020-12-19 02:21:07 +0100

    Updates

---
+++ precache-manifest.899b1dc435cb0915efbab4150c2c909b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00815b24563c2b3e4e93c111045a905f",
+    "url": "/TexasHoldemOdds/index.html"
+  },
+  {
+    "revision": "2474f0edd6522d6f58ae",
+    "url": "/TexasHoldemOdds/static/css/main.81af8fc2.chunk.css"
+  },
+  {
+    "revision": "5effca004d70018bfca8",
+    "url": "/TexasHoldemOdds/static/js/2.5cdeef29.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/TexasHoldemOdds/static/js/2.5cdeef29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2474f0edd6522d6f58ae",
+    "url": "/TexasHoldemOdds/static/js/main.b1a1219f.chunk.js"
+  },
+  {
+    "revision": "d0a26294a39e573c77d4",
+    "url": "/TexasHoldemOdds/static/js/runtime-main.a94e5640.js"
+  }
+]);
